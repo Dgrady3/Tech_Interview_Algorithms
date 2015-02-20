22e537c022a2944ac07327f41849cd20dcb3403f
Dgrady3 <joegrady1@gmail.com>
2015-02-20 12:20:37 -0500

    Reversed a string function

---
+++ reverse_string.rb
@@ -0,0 +1,13 @@
+# Write an iterative function to reverse a string. Do the same thing as a recursive function.
+
+def reverse_reverse(word)
+  array = []
+
+  word.each_char {|a| array << a }
+
+  array.reverse!
+
+  p array.join
+end
+
+reverse_reverse("Yankees suck")
