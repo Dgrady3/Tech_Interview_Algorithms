99abc4ab193941f6d750c3bb26e83041f0591c40
Dgrady3 <joegrady1@gmail.com>
2015-02-19 22:13:10 -0500

    Fionacci algorithm

---
+++ fibonacci.rb
@@ -0,0 +1,17 @@
+# # The Fibonacci Sequence is the series of numbers:
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+# The next number is found by adding up the two numbers before it.
+
+# The 2 is found by adding the two numbers before it (1+1)
+# Similarly, the 3 is found by adding the two numbers before it (1+2),
+# And the 5 is (2+3),
+# and so on!
+
+def fib(n)
+  return n if n <= 1
+  fib(n-1) + fib(n-2)
+end
+
+p fib(10)
