c72b87397b0c8a1d33c2fc428d56a15f81d0e1f6
Dgrady3 <joegrady1@gmail.com>
2015-02-28 17:40:30 -0500

    added comments

--- before_hook_rspec.rb
+++ before_hook_rspec.rb
@@ -1,20 +1,22 @@
 # Refactor the zombie spec to use a before hook.
 # For bonus points refactor the examples to one line.
 
-# describe Zombie do
-#   let(:zombie) { Zombie.new }
-#   subject { zombie }
+describe Zombie do
+  let(:zombie) { Zombie.new }
+  subject { zombie }
 
-#   it 'is not a dummy zombie' do
-#     zombie.eat_brains
-#     zombie.should_not be_dummy
-#   end
+  it 'is not a dummy zombie' do
+    zombie.eat_brains
+    zombie.should_not be_dummy
+  end
 
-#   it 'is a genius zombie' do
-#     zombie.eat_brains
-#     zombie.should be_genius
-#   end
-# end
+  it 'is a genius zombie' do
+    zombie.eat_brains
+    zombie.should be_genius
+  end
+end
+
+### Refactored below ###
 
 describe Zombie do
   let(:zombie) { Zombie.new }
