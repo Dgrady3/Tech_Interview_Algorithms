fb6d8042289380b19fa4e955431d68d3e572d1e9
Dgrady3 <joegrady1@gmail.com>
2015-03-03 16:02:48 -0500

    Added step 4

--- matcher_avalibility_rspec.rb
+++ matcher_avalibility_rspec.rb
@@ -2,7 +2,9 @@
 # For starters, let's configure RSpec to include our new module only for models.
 
 
-### Step one (lines added on 33) ###
+# step 0
+
+### Set up (lines added on 33) ###
 module ValidateNumericalityOf
   class Matcher
     def initialize(attribute)
@@ -34,6 +36,7 @@ RSpec.configure do |config|
   config.include ValidateNumericalityOf, type: :model
 end
 
+# step 1
 
 # In our spec, we're calling the validate_numericality_of
 # method that is defined in our matcher. Update this method
@@ -71,6 +74,7 @@ RSpec.configure do |config|
   config.include ValidateNumericalityOf, type: :model
 end
 
+# step 2
 
 # It's time to implement our matches? method which will do the real work here.
 # We've done most of the work for you in the check_for_number method.
@@ -115,4 +119,49 @@ RSpec.configure do |config|
   config.include ValidateNumericalityOf, type: :model
 end
 
+
+#step 3
+
+# When our spec runs, we'll see the description print as the spec
+# name in red or green, depending on if our spec passes. Let's make
+# this more informative by having it return a message indicating that
+# it validates numericality of the passed in attribute.
+
+module ValidateNumericalityOf
+  class Matcher
+    def initialize(attribute)
+      @attribute = attribute
+    end
+
+    def matches?(model)
+      check_for_number model
+    end
+
+    def failure_message
+      "Not implemented"
+    end
+
+    def negative_failure_message
+      "Not implemented"
+    end
+
+    def description
+      "Not implemented"
+    end
+
+    def check_for_number model
+      model[@attribute] = "not a number"
+      model.valid?
+      model.errors[@attribute].include?("is not a number")
+    end
+  end
+
+  def validate_numericality_of(attribute)
+    Matcher.new(attribute)
+  end
+end
+
+RSpec.configure do |config|
+  config.include ValidateNumericalityOf, type: :model
+end
 
