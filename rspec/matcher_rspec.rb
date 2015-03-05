96783e98cc23b08f87ad76348c1d403c45b4d4b6
Dgrady3 <joegrady1@gmail.com>
2015-03-05 17:22:06 -0500

    Added matcher for solution

---
+++ rspec/matcher_rspec.rb
@@ -0,0 +1,19 @@
+# Using a predicate 'be' matcher, make sure that a tweet
+# like "Nom nom nom" (which is not a reply because it doesn't
+# start with an '@' sign) is public.
+
+describe Tweet do
+  it 'without a leading @ symbol should be public' do
+    tweet = Tweet.new(status: 'Nom nom nom')
+  end
+end
+
+
+### solution below ###
+
+describe Tweet do
+  it 'without a leading @ symbol should be public' do
+    tweet = Tweet.new(status: 'Nom nom nom')
+    tweet.should be_public
+  end
+end
