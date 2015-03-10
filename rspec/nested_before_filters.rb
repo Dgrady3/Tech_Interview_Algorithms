d3a6736b10552d07b5844b22c6b666fe10f9b699
Dgrady3 <joegrady1@gmail.com>
2015-03-10 19:44:06 -0400

    Added specific before filter within second context

--- rspec/nested_before_filters.rb
+++ rspec/nested_before_filters.rb
@@ -1,6 +1,6 @@
-# We've decided to refactor out the smart zombie
-# into its own context. Add a before filter specific
-# to this context that will make our smart zombie spec pass.
+Weve decided to refactor out the smart zombie
+into its own context. Add a before filter specific
+to this context that will make our smart zombie spec pass.
 
 describe Zombie do
   let(:zombie) { Zombie.new }
@@ -16,18 +16,17 @@ describe Zombie do
 end
 
 
-
+# refactored below #
 
 describe Zombie do
   let(:zombie) { Zombie.new }
   before { zombie.iq = 0 }
   subject { zombie }
 
-
   it { should be_dummy }
 
   context 'with a smart zombie' do
-    before { zombie.iq = 3 }
+    before { zombie.iq = 3}
     it { should_not be_dummy }
   end
 end
